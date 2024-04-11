8fc4faecfc250076ce03f821e7fc57c4c0cdb550
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-04-11 19:32:08 +0200

    CouponCreate implementation

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -35,5 +35,32 @@ namespace Mango.Web.Controllers
             // Render the view with the list of coupons
             return View(list);
         }
+
+        // Action method for displaying the coupon creation form
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        // Action method for handling coupon creation form submission
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid )
+            {
+                // Call the service method to create a new coupon asynchronously
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+
+                // Check if the coupon creation was successful
+                if (response != null && response.IsSuccess)
+                {
+                    // Redirect to the coupon index view upon successful creation
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+
+            // If model state is not valid or creation was unsuccessful, return to the create view with the model
+            return View(model);
+        }
     }
 }
