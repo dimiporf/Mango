008f668827b98bad44281d9b977e5ded0767fba2
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-04-16 19:51:58 +0200

    Fixed nuget EntityCore

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    // Represents the application's database context
+    public class AppDbContext : IdentityDbContext
+    {
+        // Initializes a new instance of the AppDbContext class
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Configures the model for the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
