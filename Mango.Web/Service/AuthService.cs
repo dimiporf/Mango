db963e23c2f7a7c3f1862024941f801eac1c3bbc
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-04-22 17:26:53 +0200

    LoginAsync, RegisterAsync, AssignRoleAsync Endpoints implemented for AuthAPI

---
+++ Mango.Web/Service/AuthService.cs
@@ -0,0 +1,51 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    // Represents the implementation of the authentication service.
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        // Initializes a new instance of the AuthService class with the specified base service.
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        // Asynchronously assigns a role to a user based on the provided registration details.
+        public async Task<ResponseDto?> AssingRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        // Asynchronously performs user login based on the provided login request.
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = loginRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/login"
+            });
+        }
+
+        // Asynchronously registers a new user based on the provided registration details.
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/register"
+            });
+        }
+    }
+}
---
+++ Mango.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    // Represents the service contract for authentication operations.
+    public interface IAuthService
+    {
+        // Asynchronously performs user login based on the provided credentials.
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+
+        // Asynchronously registers a new user based on the provided registration details.
+        Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+
+        // Asynchronously assigns a role to a user based on the registration request details.
+        Task<ResponseDto?> AssingRoleAsync(RegistrationRequestDto registrationRequestDto);
+    }
+}
+
