3189be4a7b51f3e299c6e10c9d7fb1b7f6f11917
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-04-27 20:38:47 +0200

    Moved Authentication Services into a seperate, static class

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -62,42 +63,8 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
-// Parse the ApiSettings Section into a variable to demonstrate 'GetSection' method
-var settingsSection = builder.Configuration.GetSection("ApiSettings");
-
-
-// Retrieve JWT secret, issuer, and audience from configuration
-var secret = settingsSection.GetValue<string>("Secret");
-var issuer = settingsSection.GetValue<string>("Issuer");
-var audience = settingsSection.GetValue<string>("Audience");
-
-// Convert the secret into a byte array
-var key = Encoding.ASCII.GetBytes(secret);
-
-// Add JWT authentication services to the DI container
-builder.Services.AddAuthentication(x =>
-{
-    // Specify the default authentication scheme for both authentication and challenge responses
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    // Configure the JWT bearer options
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        // Enable validation of the JWT signature using the specified key
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-
-        // Validate the JWT issuer (if specified) against the expected issuer
-        ValidateIssuer = true,
-        ValidIssuer = issuer,
-
-        // Validate the JWT audience (if specified) against the expected audience
-        ValidAudience = audience,
-        ValidateAudience = true
-    };
-});
+// Invoke services for authentication that we moved into a seperate class
+builder.AddAppAuthentication();
 
 // Add authorization services to the DI container
 builder.Services.AddAuthorization();
