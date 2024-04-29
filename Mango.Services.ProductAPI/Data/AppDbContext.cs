595b3dc90eeca9d4ac06529834e9166b3a13f248
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-04-29 19:28:51 +0200

    ProductDto & SeedData

---
+++ Mango.Services.ProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Data
+{
+    // Represents the application's database context
+    public class AppDbContext : DbContext
+    {
+        // Initializes a new instance of the AppDbContext class
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Gets or sets the DbSet for coupons
+        public DbSet<Product> Products { get; set; }
+
+        // Configures the model for the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed initial data for coupons
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Samosa",
+                Price = 15,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/603x403",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Paneer Tikka",
+                Price = 13.99,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/602x402",
+                CategoryName = "Appetizer"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Sweet Pie",
+                Price = 10.99,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/601x401",
+                CategoryName = "Dessert"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Pav Bhaji",
+                Price = 15,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/600x400",
+                CategoryName = "Entree"
+            });
+        }
+    }
+}
