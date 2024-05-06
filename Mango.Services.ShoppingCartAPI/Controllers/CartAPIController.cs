99a07ba085dfdb37d503e6535e684653d9fc853e
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-05-06 12:58:55 +0200

    Controller endpoint for Upsert /Cart

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    // Represents the controller for managing shopping cart operations via API endpoints.
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response; // Holds the response data for API operations.
+        private IMapper _mapper; // Handles object mapping operations.
+        private readonly AppDbContext _db; // Represents the application database context.
+
+        // Initializes a new instance of the CartAPIController class.
+        public CartAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            this._response = new ResponseDto(); // Initialize the response object.
+            _mapper = mapper; // Assign the mapper for object mapping operations.
+        }
+
+        // Handles the HTTP POST request to upsert a shopping cart.
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> Upsert(CartDto cartDto)
+        {
+            // Implementation of the cart upsert operation will go here.
+        }
+    }
+}
