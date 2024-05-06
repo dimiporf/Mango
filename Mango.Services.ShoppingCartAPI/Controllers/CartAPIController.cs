c803cf602e1a7c49007f124929203cca83ec7ae6
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-05-06 13:06:15 +0200

    First approach of CartUpsert based on initial design

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -3,6 +3,7 @@ using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -25,9 +26,36 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
 
         // Handles the HTTP POST request to upsert a shopping cart.
         [HttpPost("CartUpsert")]
-        public async Task<ResponseDto> Upsert(CartDto cartDto)
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
-            // Implementation of the cart upsert operation will go here.
+            try
+            {
+                var cartHeaderFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u=>u.UserId == cartDto.CartHeader.UserId);
+                if (cartHeaderFromDb == null)
+                {
+                    // create header and details
+                }
+                else
+                {
+                    // check for same products
+                    var cartDetailsFromDb = await _db.CartDetails.FirstOrDefaultAsync(
+                        u => u.ProductId == cartDto.CartDetails.First().ProductId &&
+                        u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+                    if (cartDetailsFromDb == null)
+                    {
+                        // create cartdetails
+                    }
+                    else
+                    {
+                        // update count in cart details
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message.ToString();
+                _response.IsSuccess = false;
+            }
         }
     }
 }
