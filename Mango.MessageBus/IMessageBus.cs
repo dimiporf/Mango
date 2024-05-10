719e78dc7f20bbbd5aa8a0ab587894a5314ddfd2
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-05-10 21:23:48 +0200

    MessageBus initial Interface

---
+++ Mango.MessageBus/IMessageBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    // This interface represents a message bus, which is responsible for handling message publication.
+    public interface IMessageBus
+    {
+        // Asynchronously publishes a message to a specified topic or queue.
+        // The 'message' parameter is the object representing the message to be published.
+        // The 'topic_queue_Name' parameter specifies the name of the topic or queue to which the message should be published.
+        Task PublishMessage(object message, string topic_queue_Name);
+    }
+}
+
