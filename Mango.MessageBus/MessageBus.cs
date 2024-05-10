a6b9d7b18d4d373901f3666e9f47d0efc48d0828
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-05-10 21:32:59 +0200

    Added implementation for MessageBus

--- Mango.MessageBus/MessageBus.cs
+++ Mango.MessageBus/MessageBus.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mango.MessageBus
 {
-    internal class MessageBus
+    // This class implements the IMessageBus interface and provides functionality for publishing messages to Azure Service Bus.
+    public class MessageBus : IMessageBus
     {
+        // The connection string used to connect to Azure Service Bus.
+        private string connectionString = "Endpoint=sb://dimweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+BrelkVwILrxNyaraaT3qgrCVAK8cPaC9+ASbAlvQ0k=";
+
+        // Publishes a message to a specified topic or queue in Azure Service Bus.
+        // The 'message' parameter is the object representing the message to be published.
+        // The 'topic_queue_Name' parameter specifies the name of the topic or queue to which the message should be published.
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            // Create a new ServiceBusClient instance using the specified connection string.
+            await using var client = new ServiceBusClient(connectionString);
+
+            // Create a ServiceBusSender for the specified topic or queue.
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            // Serialize the message object to JSON format.
+            var jsonMessage = JsonConvert.SerializeObject(message);
+
+            // Create a ServiceBusMessage with the serialized JSON message payload.
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                // Set a unique CorrelationId for the message.
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            // Send the message asynchronously to the specified topic or queue using the sender.
+            await sender.SendMessageAsync(finalMessage);
+
+            // Dispose of the ServiceBusClient instance after sending the message.
+            await client.DisposeAsync();
+        }
     }
 }
+
